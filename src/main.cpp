d2d1fab9891ef81a0486cca2c0fb68d9ab84c736
Nick Winters <nwinters3000@gmail.com>
2020-01-18 22:05:18 -0800

    globally allocated "messages" array instead of malloc/free. Also use less dynamic memory by using F()

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,15 @@
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = { 
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEE };
-IPAddress ip(10,18,0, 48);
+    0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEE};
+IPAddress ip(10, 18, 0, 48);
 
 char cmd[15];
 char param1[15];
 boolean systemArmed = true;
 
-typedef struct {
+typedef struct
+{
   int screenDown;
   int projectorOn;
   int currentInput;
@@ -20,11 +21,14 @@ typedef struct {
 
 #define STRING_BUFFER_SIZE 128
 char buffer[STRING_BUFFER_SIZE];
+#define MESSAGES_BUFFER_SIZE 32
+char * messages[MESSAGES_BUFFER_SIZE]; // split list of http path parts
+char emptystring[] = ""; // used to initialize messages
 
 EthernetServer server(80);
 IRsend irsend;
 
-SYSTEM_STATE projector = { -1, -1, -1};
+SYSTEM_STATE projector = {-1, -1, -1};
 
 #define upPin 8
 #define downPin 7
@@ -52,8 +56,8 @@ SYSTEM_STATE projector = { -1, -1, -1};
 #define HDMI1	0x4CB36897
 #define HDMI2	0x4CB30CF3
 
-
-void setup() {
+void setup()
+{
   pinMode(upPin, OUTPUT);
   pinMode(downPin, OUTPUT);
   digitalWrite(upPin, LOW);
@@ -63,279 +67,358 @@ void setup() {
   // start the Ethernet connection and the server:
   Ethernet.begin(mac, ip);
   server.begin();
-  Serial.print("server is at ");
+  Serial.print(F("server is at "));
   Serial.println(Ethernet.localIP());
   pinMode(5, OUTPUT);
   digitalWrite(5, LOW);
 }
 
-void send404(EthernetClient client) {
-     client.println("HTTP/1.1 404 OK");
-     client.println("Content-Type: text/html");
-     client.println("Connnection: close");
+void send404(EthernetClient client)
+{
+  client.println(F("HTTP/1.1 404 OK"));
+  client.println(F("Content-Type: text/html"));
+  client.println(F("Connnection: close"));
      client.println();
-     client.println("<!DOCTYPE HTML>");
-     client.println("<html><body>404</body></html>");
+  client.println(F("<!DOCTYPE HTML>"));
+  client.println(F("<html><body>404</body></html>"));
 }
 
-void sendStatus(EthernetClient client) {
-      client.println("HTTP/1.1 200 OK");
-      client.println("Content-Type: application/json");
-      client.println("Connnection: close");
+void sendStatus(EthernetClient client)
+{
+  client.println(F("HTTP/1.1 200 OK"));
+  client.println(F("Content-Type: application/json"));
+  client.println(F("Connnection: close"));
       client.println();
       
-      client.print("{\"ProjectorState\":");
-      if(projector.screenDown) {
-        client.print("true");
+  client.print(F("{\"ProjectorState\":"));
+  if (projector.screenDown)
+  {
+    client.print(F("true"));
       }
-      else {
-        client.print("false");
+  else
+  {
+    client.print(F("false"));
       }
      
-      client.print(", \"ScreenState\":");
-      if(projector.projectorOn) {
-        client.print("true");
+  client.print(F(", \"ScreenState\":"));
+  if (projector.projectorOn)
+  {
+    client.print(F("true"));
       }
-      else{
-        client.print("false");
+  else
+  {
+    client.print(F("false"));
       }
-      client.print("}");
+  client.print(F("}"));
 }
 
-void sendHelp(EthernetClient client) {
+// command list
+// replaces string literals that live in ram with flash
+// so MAKE_COMMAND(foo); lets you replace "foo" with CMD_foo
+// I'd like PROGMEM in here but that doesn't seem to work directly
+// (I'd have to make reference thing that copies from flash to ram buffer)
+#define MAKE_COMMAND(name) const char CMD_##name[] = #name
+MAKE_COMMAND(help);
+MAKE_COMMAND(status);
+MAKE_COMMAND(on);
+MAKE_COMMAND(off);
+MAKE_COMMAND(user1);
+MAKE_COMMAND(user2);
+MAKE_COMMAND(user3);
+MAKE_COMMAND(brightness);
+MAKE_COMMAND(contrast);
+MAKE_COMMAND(mode);
+MAKE_COMMAND(keystone);
+MAKE_COMMAND(aspect);
+MAKE_COMMAND(3d);
+MAKE_COMMAND(dynamicblack);
+MAKE_COMMAND(sleeptimer);
+MAKE_COMMAND(enter);
+MAKE_COMMAND(menu);
+MAKE_COMMAND(upvolume);
+MAKE_COMMAND(dnvolume);
+MAKE_COMMAND(ltsource);
+MAKE_COMMAND(rtresync);
+MAKE_COMMAND(hdmi1);
+MAKE_COMMAND(hdmi2);
+MAKE_COMMAND(screendown);
+MAKE_COMMAND(screenup);
+MAKE_COMMAND(mute);
  
+void sendHelp(EthernetClient client)
+{
+
    // Send a standard http response header
-   client.println("HTTP/1.1 200 OK");
-   client.println("Content-Type: application/json");
-   client.println("Connnection: close");
+  client.println(F("HTTP/1.1 200 OK"));
+  client.println(F("Content-Type: application/json"));
+  client.println(F("Connnection: close"));
    client.println();
 
-   client.println("Available Commands:");
-   client.println("status");
-   client.println("on");
-   client.println("off");
-   client.println("user1");
-   client.println("user2");
-   client.println("user3");
-   client.println("brightness");
-   client.println("contrast");
-   client.println("mode");
-   client.println("mode");
-   client.println("keystone");
-   client.println("aspect");
-   client.println("3d");
-   client.println("dynamicblack");
-   client.println("sleeptimer");   
-   client.println("enter");
-   client.println("menu");
-   client.println("upvolume+");
-   client.println("dnvolume-");
-   client.println("ltsource");
-   client.println("rtresync");
-   client.println("hdmi1");
-   client.println("hdmi2");
-   client.println("screendown");
-   client.println("screenup");
-  
+  client.println(F("Available Commands:"));
+  Serial.println(CMD_help);
+  client.println(CMD_help);
+  client.println(CMD_status);
+  client.println(CMD_on);
+  client.println(CMD_off);
+  client.println(CMD_user1);
+  client.println(CMD_user2);
+  client.println(CMD_user3);
+  client.println(CMD_brightness);
+  client.println(CMD_contrast);
+  client.println(CMD_mode);
+  client.println(CMD_keystone);
+  client.println(CMD_aspect);
+  client.println(CMD_3d);
+  client.println(CMD_dynamicblack);
+  client.println(CMD_sleeptimer);
+  client.println(CMD_enter);
+  client.println(CMD_menu);
+  client.println(CMD_upvolume);
+  client.println(CMD_dnvolume);
+  client.println(CMD_ltsource);
+  client.println(CMD_rtresync);
+  client.println(CMD_hdmi1);
+  client.println(CMD_hdmi2);
+  client.println(CMD_screendown);
+  client.println(CMD_screenup);
+  client.println(CMD_mute);
 }
-
 
 /**
  * Parse the string and return an array which contains all path segments
  */
-char** parse(char* str) {
-
-	char ** messages;
-	messages = (char**)malloc(sizeof(char *));
+void parse(char *str)
+{
+  for (int i = 0; i < MESSAGES_BUFFER_SIZE; i++) {
+    messages[i] = emptystring;
+  }
 	char *p;
 	p = strtok(str, " ");
 	unsigned int i = 0;
-	while (p != NULL) {
+  while (p != NULL)
+  {
 	  p = strtok(NULL, "/");
 	  char *sp;
 	  boolean last = false;
 	  sp = strchr(p, ' ');
-	  if (sp != NULL) {
+    if (sp != NULL)
+    {
             *sp++ = '\0';
 	    last = true;
 	  }
 	  messages[i] = p;
 	  i++;
-	  if (last) {
+    if (last)
+    {
+      break;
+    }
+    if (i +1 >= MESSAGES_BUFFER_SIZE) 
+    { 
 	    break;
 	  }
-	  messages = (char**)realloc(messages, sizeof(char *) * i + 1);
 	}
 
 	messages[i] = '\0';
-	return messages;
 }
 
-void setScreenState(bool down){
-  if(down){
+void setScreenState(bool down)
+{
+  if (down)
+  {
     projector.screenDown = 1;
     digitalWrite(downPin, HIGH);
     delay(1000);
     digitalWrite(downPin, LOW);
   }
-  else{
+  else
+  {
     projector.screenDown = 0;
     digitalWrite(upPin, HIGH);
     delay(1000);
     digitalWrite(upPin, LOW);
   }
-
 }
 
-void handleCommand(EthernetClient client, char* cmd, char* param) {
-    if (strcmp(cmd, "help") == 0) {
+void handleCommand(const EthernetClient &client, char *cmd, char *param)
+{
+  if (strcmp(cmd, "help") == 0)
+  {
       sendHelp(client);
       
-      Serial.println("help");
+    Serial.println(F("help"));
     }  
-    else if (strcmp(cmd, "on") == 0) {
+  else if (strcmp(cmd, CMD_on) == 0)
+  {
       irsend.sendNEC(on, 32);
-      Serial.println("on");
+    Serial.println(F("on"));
       projector.projectorOn = 1;
       setScreenState(true);
       sendStatus(client);
     } 
     
-    else if (strcmp(cmd, "off") == 0) {
+  else if (strcmp(cmd, CMD_off) == 0)
+  {
       irsend.sendNEC(off, 32);
       delay(900);
       irsend.sendNEC(off, 32);
-      Serial.println("off");
+    Serial.println(F("off"));
       projector.projectorOn = 0;
       setScreenState(false);
       sendStatus(client);
     }
-    else if (strcmp(cmd, "user1") == 0) {
+  else if (strcmp(cmd, CMD_user1) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(USER1, 32);
       Serial.println(cmd);
     }  
-    else if (strcmp(cmd, "user2") == 0) {
+  else if (strcmp(cmd, CMD_user2) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(USER2, 32);
       Serial.println(cmd);
     }    
-    else if (strcmp(cmd, "user3") == 0) {
+  else if (strcmp(cmd, CMD_user3) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(USER3, 32);
       Serial.println(cmd);
     }      
-    else if (strcmp(cmd, "brightness") == 0) {
+  else if (strcmp(cmd, CMD_brightness) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(BRIGHTNESS, 32);
       Serial.println(cmd);
     } 
-    else if (strcmp(cmd, "contrast") == 0) {
+  else if (strcmp(cmd, CMD_contrast) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(CONTRAST, 32);
       Serial.println(cmd);
     } 
-    else if (strcmp(cmd, "mode") == 0) {
+  else if (strcmp(cmd, CMD_mode) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(MODE, 32);
       Serial.println(cmd);
     }
-    else if (strcmp(cmd, "keystone") == 0) {
+  else if (strcmp(cmd, CMD_keystone) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(KEYSTONE, 32);
       Serial.println(cmd);
     }
-    else if (strcmp(cmd, "aspect") == 0) {
+  else if (strcmp(cmd, CMD_aspect) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(ASPECT, 32);
       Serial.println(cmd);
     }
-    else if (strcmp(cmd, "3d") == 0) {
+  else if (strcmp(cmd, CMD_3d) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(THREED, 32);
       Serial.println(cmd);
     }
-    else if (strcmp(cmd, "mute") == 0) {
+  else if (strcmp(cmd, CMD_mute) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(MUTE, 32);
       Serial.println(cmd);
     }
-    else if (strcmp(cmd, "dynamicblack") == 0) {
+  else if (strcmp(cmd, CMD_dynamicblack) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(DynamicBlack, 32);
       Serial.println(cmd);
     }
-    else if (strcmp(cmd, "sleeptimer") == 0) {
+  else if (strcmp(cmd, CMD_sleeptimer) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(SLEEPTIMER, 32);
       Serial.println(cmd);
     }
-    else if (strcmp(cmd, "enter") == 0) {
+  else if (strcmp(cmd, CMD_enter) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(ENTER, 32);
       Serial.println(cmd);
     }
-    else if (strcmp(cmd, "menu") == 0) {
+  else if (strcmp(cmd, CMD_menu) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(MENU, 32);
       Serial.println(cmd);
     }
-    else if (strcmp(cmd, "upvolume+") == 0) {
+  else if (strcmp(cmd, CMD_upvolume) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(UPVOLUME, 32);
       Serial.println(cmd);
     }
-    else if (strcmp(cmd, "dnvolume-") == 0) {
+  else if (strcmp(cmd, CMD_dnvolume) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(DNVOLUME, 32);
       Serial.println(cmd);
     }
-    else if (strcmp(cmd, "ltsource") == 0) {
+  else if (strcmp(cmd, CMD_ltsource) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(LTSOURCE, 32);
       Serial.println(cmd);
     }
-    else if (strcmp(cmd, "rtresync") == 0) {
+  else if (strcmp(cmd, CMD_rtresync) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(RTRESYNC, 32);
       Serial.println(cmd);
     }
-    else if (strcmp(cmd, "hdmi1") == 0) {
+  else if (strcmp(cmd, CMD_hdmi1) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(HDMI1, 32);
       Serial.println(cmd);
     }
-    else if (strcmp(cmd, "hdmi2") == 0) {
+  else if (strcmp(cmd, CMD_hdmi2) == 0)
+  {
       sendStatus(client);
       irsend.sendNEC(HDMI2, 32);
       Serial.println(cmd);
     } 
-    else if(strcmp(cmd, "status") == 0){
+  else if (strcmp(cmd, CMD_status) == 0)
+  {
       sendStatus(client);
     }
-    else if(strcmp(cmd, "screendown") == 0){
+  else if (strcmp(cmd, CMD_screendown) == 0)
+  {
       setScreenState(true);
       projector.screenDown = 1;
     }
-    else if(strcmp(cmd, "screenup") == 0){
+  else if (strcmp(cmd, CMD_screenup) == 0)
+  {
       irsend.sendNEC(off, 32);
       delay(900);
       irsend.sendNEC(off, 32);
-      Serial.println("off");
+    Serial.println(F("off"));
       projector.projectorOn = 0;
       projector.screenDown = 0;
       setScreenState(false);
     }
-    else {
+  else
+  {
       send404(client);
     }  
 }
 
-int countSegments(char* str) {
+int countSegments(char *str)
+{
   int p = 0;
   int count = 0;
-  while (str[p] != '\0') {
-    if (str[p] == '/') {
+  while (str[p] != '\0')
+  {
+    if (str[p] == '/')
+    {
       count++;
     }
     p++;
@@ -345,14 +428,18 @@ int countSegments(char* str) {
   return count;
 }
 
-void loop() {
+void loop()
+{
   // listen for incoming clients
   EthernetClient client = server.available();
-  if (client) {
+  if (client)
+  {
     // an http request ends with a blank line
     boolean currentLineIsBlank = true;
-    while (client.connected()) {
-      if (client.available()) {
+    while (client.connected())
+    {
+      if (client.available())
+      {
         
         char c;
         int bufindex = 0; // reset buffer
@@ -360,10 +447,12 @@ void loop() {
         buffer[1] = client.read();
         bufindex = 2;
         // Read the first line to determin the request page
-        while (buffer[bufindex-2] != '\r' && buffer[bufindex-1] != '\n') { 
+        while (buffer[bufindex - 2] != '\r' && buffer[bufindex - 1] != '\n')
+        {
             // read full row and save it in buffer
             c = client.read();
-            if (bufindex<STRING_BUFFER_SIZE) {
+          if (bufindex < STRING_BUFFER_SIZE)
+          {
               buffer[bufindex] = c;
             }
             bufindex++;
@@ -373,23 +462,26 @@ void loop() {
  
         // Parse the query string
         int nSegments = countSegments(buffer);
-        char **pathsegments = parse(buffer);
+        parse(buffer);
  
         int i = 0;
-        for(i=0; i<nSegments; i++) {
-          Serial.println(pathsegments[i]);
+        for (i = 0; i < nSegments; i++)
+        {
+          Serial.println(messages[i]);
         }
 
-        if (c == '\n' && currentLineIsBlank) {
-          handleCommand(client, pathsegments[0], pathsegments[1]);
+        if (c == '\n' && currentLineIsBlank)
+        {
+          handleCommand(client, messages[0], messages[1]);
           break;
         }
 
-        free(pathsegments);
-
-        if (c == '\n') {
+        if (c == '\n')
+        {
           currentLineIsBlank = true;
-        } else if (c != '\r') {
+        }
+        else if (c != '\r')
+        {
           currentLineIsBlank = false;
         }
       }
@@ -398,6 +490,6 @@ void loop() {
     delay(1);
     // Close the connection:
     client.stop();
-    Serial.println("Client disonnected");
+    Serial.println(F("Client disonnected"));
   }
 }
